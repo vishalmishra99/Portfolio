1e6bc753b1775691226c93c267da096bb859eaa5
respectvishal2000 <respectvishal2000@gmail.com>
2024-06-19 00:48:03 +0530

    my changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/profile.jpg";
-import CV from "../../assets/mycv.pdf";
+import CV from "../../assets/Vishal_Profile.pdf";
 import Info from "./Info";
 
 const About = () => {
